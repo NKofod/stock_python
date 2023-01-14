4a14be07ea0e43264bcb1d7eb4962eb770eb1740
Nicolai J. Kofod-Jensen <nicolai.kofod.jensen@gmail.com>
2023-01-14 01:01:19 +0100

    Auto-update

--- fetch.py
+++ fetch.py
@@ -1,29 +1,32 @@
 
 import pandas as pd
 import json 
-import pandas_datareader.data as web 
 import datetime 
 import time 
+import alpha_vantage.timeseries
 
 with open('env.json', "r") as infile:
     env_json = json.load(infile)
     
 with open('stocks.json','r') as infile:
     stocks_dict = json.load(infile)
-    
+if env_json['Current_day'] > 14: 
+    env_json['Current_day'] = 1 
 stock_list = stocks_dict[str(env_json['Current_day'])]
-
+    
+ts = alpha_vantage.timeseries.TimeSeries(key='QVEGLYRJUK0ABVMT',output_format='pandas')
 day = datetime.date.today().day 
 month = datetime.date.today().month 
 year = datetime.date.today().year
 for i in stock_list: 
     try: 
-        tmp = web.DataReader(i , 'av-intraday', start=datetime.datetime(2022,1,1),end=datetime.date.today(),api_key='QVEGLYRJUK0ABVMT')
-        ticker_list = [i] * len(tmp)
-        tmp['Ticker'] = ticker_list 
-        tmp.to_csv(f'./data/{i}_{year}_{month}_{day}.csv')
+        data, meta_data = ts.get_intraday('GOO','1min','full')
+        ticker_list = [i] * len(data)
+        data['Ticker'] = ticker_list 
+        data.to_csv(f'./data/{i}_{year}_{month}_{day}.csv')
     except ValueError:
         pass 
+    
     time.sleep(20)
 
 with open('env.json', "w") as outfile:
