bc39306a1843d6e4dff994a4a735a18f8e35b74b
Nicolai J. Kofod-Jensen <nicolai.kofod.jensen@gmail.com>
2023-01-28 16:01:26 +0100

    Auto-update

--- collate.py
+++ collate.py
@@ -1,13 +1,18 @@
 import pandas as pd 
 import os 
+import datetime 
+day = datetime.date.today().day 
+month = datetime.date.today().month 
+year = datetime.date.today().year
+filelist = sorted(list(os.walk("./data/"))[0][2])
+for i in [15,16,17,18,19,20,21,22]: 
+    tmp_filelist = [j for j in filelist if f"2023_1_{i}" in j]
 
-filelist = list(os.walk("./data/"))[0][2]
+    tmp_df = pd.read_csv(f"./data/{tmp_filelist[0]}",index_col=False)
 
-tmp_df = pd.read_csv(f"./data/{filelist[0]}",index_col=False)
 
-for i in filelist[1:]:
-    
-    tmp_df = pd.concat([tmp_df,pd.read_csv(f"./data/{i}",index_col=False)])
+    for j in tmp_filelist[1:]:
+        tmp_df = pd.concat([tmp_df,pd.read_csv(f"./data/{j}",index_col=False)])
 
-    print(f"{i} - {len(tmp_df)}")
-tmp_df.to_csv("./collated/collated.csv",index=False)
+        print(f"{j} - {len(tmp_df)}")
+    tmp_df.to_csv(f"./collated/collated_2023_1_{i}.csv",index=False)
--- fetch.py
+++ fetch.py
@@ -4,6 +4,7 @@ import json
 import datetime 
 import time 
 import alpha_vantage.timeseries
+import os 
 
 with open('env.json', "r") as infile:
     env_json = json.load(infile)
@@ -19,12 +20,15 @@ day = datetime.date.today().day
 month = datetime.date.today().month 
 year = datetime.date.today().year
 for i in stock_list: 
+    
     try: 
-        data, meta_data = ts.get_intraday('GOO','1min','full')
+        data, meta_data = ts.get_intraday(i,'1min','full')
         ticker_list = [i] * len(data)
         data['Ticker'] = ticker_list 
+        print(f'./data/{i}_{year}_{month}_{day}.csv')
         data.to_csv(f'./data/{i}_{year}_{month}_{day}.csv')
-    except ValueError:
+    except ValueError as e:
+        print(e.__str__())
         pass 
     
     time.sleep(20)
@@ -33,3 +37,12 @@ with open('env.json', "w") as outfile:
     env_json_out = env_json
     env_json_out['Current_day'] += 1 
     json.dump(env_json_out,outfile)
+
+filelist = list(os.walk("./data/"))[0][2]
+filelist = [i for i in filelist if f"{year}_{month}_{day}" in i]
+tmp_df = pd.read_csv(f"./data/{filelist[0]}",index_col=False)
+
+for i in filelist[1:]:
+    tmp_df = pd.concat([tmp_df,pd.read_csv(f"./data/{i}",index_col=False)])
+    print(f"{i} - {len(tmp_df)}")
+tmp_df.to_csv(f"./collated/collated_{year}_{month}_{day}.csv",index=False)
